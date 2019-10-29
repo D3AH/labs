f489dca4f4cf1a40f0429f819deb23dd3f60ff60
d3h <diegoeauyonh@gmail.com>
2019-10-28 20:15:19 -0600

    WIP Storefront

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   model() {
-    return this.store.findAll('rental');
+    return this.store.loadRecords('rental');
   }
 
   setupController(controller, model) {
--- app/pods/rentals/rental/route.js
+++ app/pods/rentals/rental/route.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class RentalsRentalRoute extends Route {
+  model({ id }) {
+    return this.store.loadRecord('rental', id);
+  }
 }
